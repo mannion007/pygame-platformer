33376757380fceba9371ffc05aa1b8a18d0ad820
James Mannion <mannion007@gmail.com>
2016-06-22 22:55:31 +0100

    Implemented get tile and draw tile methods, made the game fill the screen with random tiles

--- v1.py
+++ v1.py
@@ -1,27 +1,51 @@
 import pygame
- 
+from random import randint
+
 pygame.init()
  
 # Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
-PI = 3.141592653
-
-size = (800, 600)
-screen = pygame.display.set_mode(size)
-
+# Load assets 
 spriteSheet = pygame.image.load("assets/spritesheet.png")
- 
+
+#Config vars
+spriteSheetWidth = 320
+spriteSheetHeight = 272
+screenWidth = 480
+screenHeight = 320
+size = (screenWidth, screenHeight)
+tileSize = 16
+
+# build tile array - Todo, make this pull in a non random array
+tiles = []
+for i in range(0, 640):
+    tiles.append(randint(3,340))
+
+# Game setup
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Lovely Game")
 
- 
 # Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
+
+# get a tile from the spritesheet by number
+def getTile(tileNumber):
+    row = tileNumber / (spriteSheetWidth/tileSize)
+    column = tileNumber % (spriteSheetWidth/tileSize)
+    x = column * tileSize
+    y = row * tileSize
+    return [x,y,tileSize,tileSize]
+
+# Draw the tile array to the screen
+def drawTiles(tiles):
+    xOffset = 0
+    yOffset = 0
+    for tile in tiles:
+        if(xOffset > screenWidth):
+            xOffset = 0
+            yOffset += tileSize
+        screen.blit(spriteSheet, (xOffset, yOffset), getTile(tile))
+        xOffset += tileSize
  
 # Loop as long as done == False
 while not done:
@@ -34,10 +58,9 @@ while not done:
     # inside the main while not done loop.
  
     # Clear the screen and set the screen background
-    screen.fill(WHITE)
- 
-    #imagerect = spriteSheet.get_rect()
-    screen.blit(spriteSheet, (0,0), (64,128,16,16))
+    screen.fill([255,255,255])
+    
+    drawTiles(tiles)
  
     # Go ahead and update the screen with what we've drawn.
     # This MUST happen after all the other drawing commands.
