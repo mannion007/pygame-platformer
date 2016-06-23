f271d5edaac6aeb7bd9dc1b47259213fff8eab2d
James Mannion <mannion007@gmail.com>
2016-06-23 21:59:13 +0100

    Added scrolling controls, added generator function to get array slice of tiles to draw

--- game.py
+++ game.py
@@ -4,39 +4,39 @@ import numpy as np
 
 pygame.init()
  
-# Define some colors
-# Load assets 
+###################
+### Load assets ###
+###################
 spriteSheet = pygame.image.load("assets/spritesheet.png")
 
-#Config vars
+########################
+### Config Constants ###
+########################
 spriteSheetWidth = 320
 spriteSheetHeight = 272
 screenWidth = 480
 screenHeight = 320
 tileSize = 16
-
 levelWidth = 4800
-
-# for now assume level is horizontal only
 levelHeight = screenHeight
 
+##################
+### Setup game ###
+##################
+screen = pygame.display.set_mode([screenWidth, screenHeight])
+pygame.display.set_caption("My Lovely Game")
+clock = pygame.time.Clock()
+
+###############
+## Variables ##
+###############
+done = False
 pixelsScrolled = 0
 levelPosition = 0
 
-size = (screenWidth, screenHeight)
-
-# build the 2D numpy tile list - Todo, make this pull in a non random array
-tiles = np.ones((levelHeight/tileSize, levelWidth/tileSize), dtype=np.int)
-
-# Game setup
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Lovely Game")
-
-# Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
-
-# get a tile from the spritesheet by number
+###############
+## Functions ##
+###############
 def getTile(tileNumber):
     row = tileNumber / (spriteSheetWidth/tileSize)
     column = tileNumber % (spriteSheetWidth/tileSize)
@@ -44,46 +44,48 @@ def getTile(tileNumber):
     y = row * tileSize
     return [x,y,tileSize,tileSize]
 
-# Draw the tile array to the screen
-def drawScreenTiles(tiles, pixelsScrolled, levelPosition):
+def fetchTiles(startColumn, endColumn):
+    tilesToDraw = tiles[0:screenHeight/tileSize, startColumn:endColumn]
+    for rowCount, row in enumerate(tilesToDraw):
+        for columnCount, column in enumerate(row):
+            yield rowCount, columnCount
 
+def drawScreenTiles(tiles, pixelsScrolled, levelPosition):
     startColumn = levelPosition / tileSize
     endColumn = startColumn + (screenWidth / tileSize) + 1
-
     tilesToDraw = tiles[0:20, startColumn:endColumn]
+    for rowCount, columnCount in fetchTiles(startColumn, endColumn):
+        screen.blit(spriteSheet, (columnCount * tileSize - pixelsScrolled, rowCount * tileSize), getTile(62))
 
-    print(startColumn)
-    print(endColumn)
 
-    rowCount = 0
-    for row in tilesToDraw:
-        columnCount = 0
-        for column in row:
-            screen.blit(spriteSheet, (columnCount * tileSize - pixelsScrolled, rowCount * tileSize), getTile(62))
-            columnCount += 1
-        rowCount += 1
- 
-# Loop as long as done == False
+tiles = np.ones((levelHeight/tileSize, levelWidth/tileSize), dtype=np.int)
+
+################# 
+### Game loop ###
+#################
 while not done:
  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        
+    keystate = pygame.key.get_pressed()
+    if keystate[pygame.K_d]:
+        if(levelPosition < levelWidth):
+            levelPosition +=2
+            pixelsScrolled += 2
+            if pixelsScrolled >= tileSize:
+                pixelsScrolled = 0
+    if keystate[pygame.K_a]:
+        if(levelPosition > 0):
+            levelPosition -=2
+            pixelsScrolled -= 2
+            if pixelsScrolled <= 0:
+                pixelsScrolled = tileSize
  
     # All drawing code happens after the for loop and but
     # inside the main while not done loop.
- 
-    # Clear the screen and set the screen background
     screen.fill([255,255,255])
-
-    #tilesToDraw = tiles[0:640]
-
-    levelPosition +=1
-
-    pixelsScrolled += 1
-    if pixelsScrolled >= tileSize:
-        pixelsScrolled = 0
-
     drawScreenTiles(tiles, pixelsScrolled, levelPosition)
  
     # Go ahead and update the screen with what we've drawn.
