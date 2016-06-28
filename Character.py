4854fc381b20613e6c9e5700a031bb72ed9792d0
james.mannion <james.mannion@influentialsoftware.com>
2016-06-28 14:08:44 +0100

    Added orbitter

--- Character.py
+++ Character.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+phi = math.pi * 2
 
 class Hero:
 
@@ -17,6 +19,15 @@ class Hero:
     framesToDelay = 3
     framesDelayed = 0
     currentFrame = 0
+
+    orbitFramesDelayed = 0
+    currentOrbitFrameDelayed = 0
+
+    minOrbitRadius = 35
+    maxOrbitRadius = 300
+    orbitRadius = 35
+
+    orbitPos = [0,0]
    
     def __init__(self, screen):
         self.screenWidth, self.screenHeight = pygame.display.Info().current_w, pygame.display.Info().current_h
@@ -49,6 +60,11 @@ class Hero:
                 self.framesDelayed = 0
                 self.currentFrame = next(self.jumpGen)
 
+    def updateOrbitPos(self):
+        #self.orbitFramesDelayed += 1
+        #if(self.framesDelayed >= 5):
+            #self.orbitFramesDelayed = 0
+        self.orbitPos = self.orbit([self.position[0] + self.tileSize[0] / 2, self.position[1] + self.tileSize[1] / 2], self.orbitRadius, pygame.time.get_ticks() * 0.001)
 
     def getRunFrame(self):
         while True:
@@ -107,6 +123,13 @@ class Hero:
             if self.onGround:
                 self.state = 'stand'
 
+        if keystate[pygame.K_SPACE]:
+            if(self.orbitRadius < self.maxOrbitRadius):
+                self.orbitRadius += 2
+        else:
+            if(self.orbitRadius > self.minOrbitRadius):
+                self.orbitRadius -= 3
+
         self.speed[1] += self.gravity
         self.position[1] += self.speed[1]
         
@@ -122,6 +145,16 @@ class Hero:
             if(self.position[0] > 0):
                 self.position[0] += self.speed[0]
 
+    def orbit(self, origin, radius, time):
+        #normalise phi, to get the angle in time
+        angle = phi * time 
+        rsa = radius * math.sin(angle)
+        rca = radius * math.cos(angle)
+        return (int(origin[0] + rca), int(origin[1] + rsa))
+
     def render(self):
         self.updateHeroFrame()
+        self.updateOrbitPos()
+        
         self.screen.blit(self.spriteSheet, (self.position[0], self.position[1]), self.getTile(self.currentFrame))
+        pygame.draw.circle(self.screen, [255,255,255], self.orbitPos, self.orbitRadius / 7)
